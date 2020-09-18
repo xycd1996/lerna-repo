2d562e4c6fbbee18912158de6296f012bfea5394
cao_ming <cao_ming@hunliji.com>
2020-09-18 17:40:37 +0800

    commit

---
+++ packages/project-1/README.md
@@ -0,0 +1,11 @@
+# `project-1`
+
+> TODO: description
+
+## Usage
+
+```
+const project1 = require('project-1');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/project-1/src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        123121212
+      </div>
+    )
+  }
+}
