d8e223bb7325aed4edbb1e3140af113cfa1d2338
cao_ming <cao_ming@hunliji.com>
2020-09-28 16:02:18 +0800

    push

--- packages/project-1/src/home/index.js
+++ packages/project-1/src/home/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Cao_ming
  * @Date: 2020-09-23 17:36:30
- * @LastEditTime: 2020-09-28 14:59:40
+ * @LastEditTime: 2020-09-28 16:02:00
  * @LastEditors: Cao_ming
  * @FilePath: \lerna-repo\packages\project-1\src\home\index.js
  */
@@ -9,6 +9,6 @@ import React, { Component } from 'react'
 
 export default class index extends Component {
   render() {
-    return <div>home1111</div>
+    return <div>home1112221</div>
   }
 }
